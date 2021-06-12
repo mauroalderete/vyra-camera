618c8c0bbdf34ddd6073aed847b26fd25560d885
Rayquen <rayquen.zero@gmail.com>
2021-06-12 18:32:18 -0300

    Ejemplo simple

---
+++ monitor.py
@@ -0,0 +1,19 @@
+import cv2
+
+print("Before URL")
+cap = cv2.VideoCapture('http://admin:123456@192.168.0.240/media/?action=stream')
+print("After URL")
+
+while True:
+
+    print('About to start the Read command')
+    ret, frame = cap.read()
+    print('About to show frame of Video.')
+    cv2.imshow("Capturing",frame)
+    print('Running..')
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
