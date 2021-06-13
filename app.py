12de2c07930a9a852adabad4b680c3ac96a87c1d
Rayquen <rayquen.zero@gmail.com>
2021-06-12 23:51:12 -0300

    app and views con env data for configuration

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-from flask import Flask, render_template, Response
-from streamer import Streamer
+from flask import Flask
+from os import environ
 
-username = "admin"
-password = "123456"
-url = "192.168.0.240/media/?action=stream"
+config = { 'username': '', 'password': '', 'url': ''  }
+
+config['username'] = environ['CAM_USERNAME']
+config['password'] = environ['CAM_PASSWORD']
+config['url'] = environ['CAM_URL']
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/video')
-def video():
-    global username
-    global password
-    global url
-    
-    streamer = Streamer(username, password, url)
-    return Response(streamer.stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+import views
